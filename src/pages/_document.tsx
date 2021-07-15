d4d4f69d90639a8e8f7431006275a00eaa561afd
Pedro Dahmer <pedrohd1213@gmail.com>
2021-07-15 17:52:07 -0300

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends  Document{
 
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />                    
 
-                    {/* <link rel="shortcut icon" href="/favicon.png" type="image/png"/> */}
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
                 </Head>
 
                 <body>
