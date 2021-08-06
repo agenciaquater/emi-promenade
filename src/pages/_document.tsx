8e2da880a27755debc7fc80b46e0949d995de2f1
Pedro Dahmer <pedrohd1213@gmail.com>
2021-08-06 11:26:46 -0300

    adição das tags gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,12 @@ export default class MyDocument extends  Document{
                 </Head>
 
                 <body>
+                    <noscript>
+                        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSZ3BBF"
+                            height="0" width="0" style={{display:"none",visibility:"hidden"}}>
+                        </iframe>
+                    </noscript>
+
                     <Main />
                     <NextScript />
                 </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import { NextSeo } from 'next-seo'
 
 import { Header } from '../components/Header'
@@ -42,6 +43,16 @@ export default function Home({apikey}: IndexProps) {
           ]
         }}
       />
+
+      <Script strategy="lazyOnload">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer',"GTM-MSZ3BBF");
+        `}
+      </Script>
 
       <Head>
         <title>EMI | Promenade</title>
