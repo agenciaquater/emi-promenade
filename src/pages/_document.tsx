f92c6f5139cd85538f088d6704d21d338e4a8b3a
Pedro Dahmer <pedrohd1213@gmail.com>
2021-08-12 16:12:56 -0300

    adição da tag do google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
+import {GA_TRACKING_ID} from '../utils/gtag'
+
 export default class MyDocument extends  Document{
     render(){
         return (
@@ -7,6 +9,20 @@ export default class MyDocument extends  Document{
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                            
+                                gtag('config', '${GA_TRACKING_ID}');
+                            `
+                        }}
+                    />
                     
                     <meta name="description" content="Trabalhamos há mais de 15 anos com excelência em pós-operatório de cirurgias plásticas. Aqui você tem qualidade de vida e conforto durante sua reabilitação." />
 
